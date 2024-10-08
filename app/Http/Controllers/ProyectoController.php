085e8e919b39a27357beb7b5037d111def564e79
thedyx21 <dylan.drouet@uees.edu.ec>
2024-10-08 10:42:12 -0500

    Terminado

---
+++ app/Http/Controllers/ProyectoController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Proyecto;
+use Illuminate\Support\Facades\DB;
+
+class ProyectoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $proyectos=DB::table('proyectos')->get();
+        return view ("project/index", ['proyectos'=>$proyectos]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view ("project/new");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Proyecto::create($request->all());
+        return redirect('project/')
+           ->with('success','Proyecto creado satisfactoriamente.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $proyecto=Proyecto::find($id);
+        return view("project/update" , compact('proyecto'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'titulo' => 'required|max:255',
+            'descripcion'=> 'required'
+        ]);
+        $proyecto=Proyecto::find($id);
+        $proyecto->update($request->all());
+        return redirect('project/')
+           ->with('success','Proyecto actualizado satisfactoriamente.');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $proyecto=Proyecto::find($id);
+        $proyecto->delete();
+        return redirect('project/')
+        ->with('success', 'Proyecto eliminado satisfactoriamente');
+    }
+}
